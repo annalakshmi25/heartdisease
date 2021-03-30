c2b843c4aeda5e697f4d5686e5d57c34a860cb7f
annalakshmi25 <annalakshmiv25@gmail.com>
2021-03-30 21:59:53 +0530

    commit all files

--- hd/settings.py
+++ hd/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l!*mx%%gc549$@yqgb&p6vmukwu8lv+x58f00dzx9gom&j32*3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.222.141.214','localhost']
 
 
 # Application definition
