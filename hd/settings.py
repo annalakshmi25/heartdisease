35daa6d1eab152ff8eb954b94127a7952a39d61e
annalakshmi25 <annalakshmiv25@gmail.com>
2021-04-01 12:44:05 +0530

    edited

--- hd/settings.py
+++ hd/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l!*mx%%gc549$@yqgb&p6vmukwu8lv+x58f00dzx9gom&j32*3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.191.178.125','localhost']
+ALLOWED_HOSTS = ['18.220.178.226','localhost']
 
 
 # Application definition
